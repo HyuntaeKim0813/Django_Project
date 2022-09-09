709eb4ff6f6bbd010965df36b8b782491795cfe3
HyuntaeKim0813 <hyuntaenyc@gmail.com>
2022-09-08 22:39:19 -0400

    Initial commit

---
+++ finance_ground/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.index, name ='index'),
+    path('single/', views.single, name ='single'),
+    path('category/', views.category, name ='category'),
+    path('contact/', views.contact, name ='contact'),
+    path('investment_stock/', views.invsetment_stock, name ='investment_stock'),
+    path('investment_stock_chap1/', views.investment_stock_chap1, name ='investment_stock_chap1'),
+    path('cfa_lvl_1/', views.cfa_lvl_1, name ='cfa_lvl_1'),
+    path('cfa_lvl_1_voca/', views.cfa_lvl_1_voca, name ='cfa_lvl_1_voca'),
+    path('boston_college/', views.boston_college, name ='boston_college'),
+]
---
+++ finance_ground/views.py
@@ -0,0 +1,34 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+# request -> response
+# request handler
+# action
+
+def index(request):
+    return render(request, 'index.html')
+
+def single(request):
+    return render(request, 'single.html')
+
+def category(request):
+    return render(request, 'category.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def invsetment_stock(request):
+    return render(request, 'investment_stock.html')
+
+def investment_stock_chap1(request):
+    return render(request, 'investment_stock_chap1.html')
+
+def cfa_lvl_1(request):
+    return render(request, 'cfa_lvl_1.html')
+
+def cfa_lvl_1_voca(request):
+    return render(request, 'cfa_lvl_1_voca.html')
+
+def boston_college(request):
+    return render(request, 'boston_college.html')
